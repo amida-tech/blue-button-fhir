25ab5001a7cf3fea8a4160d3baa24068e226cd6c
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2015-02-01 14:11:58 -0500

    expose new methods in api to be used from fhir-interface

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 "use strict";
 
 var fhir = require('./lib/fhir');
+var store = require('./lib/resourceStore');
 
-exports.toModel = function (resources) {
-    return fhir.toModel(resources);
-};
+/**
+ * Translates an array of json fhir resources into blue-button json model.
+ *
+ * @param {Object[]} resources
+ */
+exports.toModel = fhir.toModel;
+
+/**
+ * Typically resources have links to other resources.  This finds top level
+ * resources for blue-button model.
+ *
+ * @param {Object[]} resources
+ */
+exports.entryResources = fhir.entryResources;
+
+/**
+ * This returns a dictionary of resources from which you can get references.
+ *
+ * @param {Object[]} resources
+ */
+exports.dictionary = store.create;
